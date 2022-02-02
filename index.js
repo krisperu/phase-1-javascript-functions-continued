25c23d8287c253e0728da836c9e23bda0779f4df
Kristina Peru <krissy.germanov@gmail.com>
2022-02-01 20:52:27 -0700

    commit tree 1fbdf8830e05ff0786bf4466d8019ce587c219f7

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+saturdayFun();
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+mondayWork();
+function mondayWork(workLocation = "go to the office") {
+    return "This Monday, I will " + workLocation + ".";
+}
+let wrapAdjective = function(style = "*") {
+    return function(adjective = "special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
